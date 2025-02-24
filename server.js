f7ee979073c8d98b8f2603fcca9b3d9d19aa36d3
SHinakami <s6502041610080@email.kmutnb.ac.th>
2025-02-24 14:46:42 +0700

    update

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const express = require('express')
+const express = require('express');
+const {PrismaClient} = require("@prisma/client");
+
 const app = express()
+const prisma =  new PrismaClient();
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/user',(req,res) =>{
+app.get('/user', async (req,res) =>{
+    const data = await prisma.user.findMany();
     res.json({
-        message: 'okay'
+        message: 'okay',
+        data
     })
 });
 
